47e03e43d082f21a53acd06e6b6e06c1bc85fc7e
RJW20 <robertjwatts84@outlook.com>
2024-03-04 17:07:38 +0000

    Implement main

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,69 @@
+import pygame
 
+from snake_app import Grid, Snake
+from snake_app.cartesian import Direction
+from settings import settings
 
 def main():
+    
+    #initialize the grid the game will be modelled from
+    grid = Grid(settings['grid_size'], settings['block_width'], settings['block_padding'])
 
-    pass
+    #pygame setup
+    screen = pygame.display.set_mode(grid.screen_size)
+    pygame.display.set_caption("Snake")
+    clock = pygame.time.Clock()
+    running = True
+
+    #initialize the snake
+    snake = Snake(settings['grid_size'], settings['start_length'])
+    snake.start_state()
+    move = snake.direction
+    speed = settings['speed']
+
+    while running:
+
+        #stops from pressing twice in one frame and then moving in forbidden directions
+        key_pressed = False
+
+        for event in pygame.event.get():
+            #so can quit
+            if event.type == pygame.QUIT: 
+                running = False
+                break
+            
+            #change direction (in allowed directions)
+            if event.type == pygame.KEYDOWN:
+                if not key_pressed:
+                    key_pressed = True
+
+                    if event.key == pygame.K_UP:
+                        move = Direction.N
+                    elif event.key == pygame.K_RIGHT:
+                        move = Direction.E
+                    elif event.key == pygame.K_DOWN:
+                        move = Direction.S
+                    elif event.key == pygame.K_LEFT:
+                        move = Direction.W
+
+        #move in the chosen direction
+        snake.move(move)
+
+        #check if we've died
+        if snake.is_dead:
+            running = False
+            break
+
+        #fill the screen to wipe last frame
+        screen.fill((40,40,40))
+
+        #visualize the snake and food on the screen
+        pygame.draw.rect(screen, 'red', pygame.Rect(grid.gridpoint_to_coordinates(snake.target.position), (grid.block_width, grid.block_width)))
+        for pos in snake.body:
+            pygame.draw.rect(screen, 'green', pygame.Rect(grid.gridpoint_to_coordinates(pos), (grid.block_width, grid.block_width)))
+
+        #display the changes
+        pygame.display.flip()
+        clock.tick(speed)
+
+    pygame.quit()
---
+++ settings.py
@@ -0,0 +1,14 @@
+
+
+settings = {
+
+    #grid properties
+    'grid_size': (30, 30),  #(width, height)
+    'block_width': 20,
+    'block_padding': 1,
+
+    #snake properties
+    'start_length': 3,     #note will be set to min(grid_size)//2 if larger than that to prevent some of the body starting outside the grid
+    'speed': 6,
+
+}
